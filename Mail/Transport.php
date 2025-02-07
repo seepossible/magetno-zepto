a806513154c59852e055da3aedf43fc1b2ce0dee
bhaveshseepossible <bhavesh@seepossible.com>
2025-02-07 12:18:36 +0530

    fix pdf attachment is not sent when it found mime instance of \Magento\Framework\Mail\MimePart class

--- Mail/Transport.php
+++ Mail/Transport.php
@@ -2,7 +2,6 @@
 
 namespace Zoho\ZeptoMail\Mail;
 
-
 use InvalidArgumentException;
 use Magento\Framework\App\ObjectManager;
 use Magento\Framework\Mail\MessageInterface;
@@ -26,29 +25,27 @@ use Zoho\ZeptoMail\Helper\ZConstants as ZConstants;
 class Transport extends MagentoTransport implements TransportInterface
 {
 
-
     protected $config;
-	
-	protected $message;
+    protected $message;
     protected $logger;
-	protected $oauthconfig;
-	protected $adminNotification;
-	protected $parameters;
-	protected $storeManager;
-	protected $storeId;
+    protected $oauthconfig;
+    protected $adminNotification;
+    protected $parameters;
+    protected $storeManager;
+    protected $storeId;
 
     public function __construct(MessageInterface $message,StoreManagerInterface $storeManager, $parameters = null, LoggerInterface $logger = null,OauthConfig $oauthConfig=null)
     {
-		$this->message = $message;
-		$this->storeManager = $storeManager;
-		$this->logger = $logger ?: ObjectManager::getInstance()->get(LoggerInterface::class);
-		$this->oauthconfig = $logger ?: ObjectManager::getInstance()->get(OauthConfig::class);
-		$this->parameters = $parameters;
-		if($storeManager->getStore()){
-			$this->storeId =$storeManager->getStore()->getId();
-		}
+        $this->message = $message;
+        $this->storeManager = $storeManager;
+        $this->logger = $logger ?: ObjectManager::getInstance()->get(LoggerInterface::class);
+        $this->oauthconfig = $logger ?: ObjectManager::getInstance()->get(OauthConfig::class);
+        $this->parameters = $parameters;
+        if($storeManager->getStore()){
+            $this->storeId =$storeManager->getStore()->getId();
+        }
         parent::__construct($message, $parameters);
-		
+
     }
 
     /**
@@ -58,156 +55,165 @@ class Transport extends MagentoTransport implements TransportInterface
      */
     public function sendMessage()
     {
-		if(!isset($this->storeId)){
-			parent::sendMessage();
-			return;
-		}
-		$domain = $this->oauthconfig->getStoreConfig('domain',$this->storeId);
-		$authtoken = base64_decode($this->oauthconfig->getStoreConfig('mail_token',$this->storeId));
-		$allowed_emails = json_decode($this->oauthconfig->getStoreConfig('allowed_emails',$this->storeId),true);
-		
-		if(!isset($domain) || !isset($authtoken)){
-			
-			parent::sendMessage();
-			return;
-		}
-		$zeptoAPI = new ZeptoMailApi($domain,$authtoken);
-		$body = $this->message->getBody();
+        if(!isset($this->storeId)){
+            parent::sendMessage();
+            return;
+        }
+        $domain = $this->oauthconfig->getStoreConfig('domain',$this->storeId);
+        $authtoken = base64_decode($this->oauthconfig->getStoreConfig('mail_token',$this->storeId));
+        $allowed_emails = json_decode($this->oauthconfig->getStoreConfig('allowed_emails',$this->storeId),true);
+
+        if(!isset($domain) || !isset($authtoken)){
+
+            parent::sendMessage();
+            return;
+        }
+        $zeptoAPI = new ZeptoMailApi($domain,$authtoken);
+        $body = $this->message->getBody();
         $parsedBody = [
             'text' => '',
-			'html' => '',
+            'html' => '',
             'attachments' => []
         ];
-
         if ($body instanceof MimeMessage || $body instanceof LaminasMimeMessage) {
             foreach ($body->getParts() as $part) {
                 if ($part instanceof MimePart || $part instanceof LaminasMimePart) {
-					if ($part->type == 'text/plain') {
-						$parsedBody['text'] .= $part->getRawContent();
-						$parsedBody['ishtml'] = false;
-					}
+                    if ($part->type == 'text/plain') {
+                        $parsedBody['text'] .= $part->getRawContent();
+                        $parsedBody['ishtml'] = false;
+                    }
                     else if ($part->type == 'text/html') {
                         $parsedBody['html'] .= $part->getRawContent();
                     } else {
                         $parsedBody['attachments'][] = [
-							'content' => base64_encode($part->getRawContent()),
+                            'content' => base64_encode($part->getRawContent()),
                             'name' => $part->filename,
                             'mime_type' => $part->type
                         ];
                     }
-                } else {
-					$parsedBody['html'] .= $part->getRawContent();
-					$parsedBody['ishtml'] = true;
-				}
+                }
+                else if($part instanceof \Magento\Framework\Mail\MimePart) {
+                    if ($part->getType() == 'text/plain') {
+                        $parsedBody['text'] .= $part->getRawContent();
+                        $parsedBody['ishtml'] = false;
+                    }
+                    else if ($part->getType() == 'text/html') {
+                        $parsedBody['html'] .= $part->getRawContent();
+                        $parsedBody['ishtml'] = true;
+                    } else {
+                        $parsedBody['attachments'][] = [
+                            'content' => base64_encode($part->getRawContent()),
+                            'name' => $part->getFilename(),
+                            'mime_type' => $part->getType()
+                        ];
+                    }
+                }
+                else {
+                    $parsedBody['html'] .= $part->getRawContent();
+                    $parsedBody['ishtml'] = true;
+                }
             }
         }else{
-		$parsedBody['ishtml'] = true;
-		$parsedBody['html'] = $body;
-	}
-		
-		
-		$message = $this->message;
-		
-		
-		$to = $cc = $bcc = $reply_to = array();
-		$headers = $message->getHeaders();
-		
-		$fromAddress =$this:: splitNameAndAddress($headers['From']);
-		
-		$mail_data = array();
-		if ($headers['From'] !== null) {
-			if (!in_array(strtolower($fromAddress['address']), $allowed_emails)){
-				$fromAddress['address'] = $this->oauthconfig->getTransmailEmailAddress(ZConstants::ident_general,$this->storeId);
-			}
-			$mail_data['from'] =  $fromAddress;
-			
+            $parsedBody['ishtml'] = true;
+            $parsedBody['html'] = $body;
         }
-		
-		if ($headers['To'] !== null) {
-			$toRecipients = $headers['To'];
-			$mail_data['to'] = $this::getEmailDetails($toRecipients);
-			
-        }
-		if (array_key_exists('Cc',$headers)) {
-			$ccRecipients = $headers['Cc'];
-			$mail_data['cc'] = $this::getEmailDetails($ccRecipients);
-			
-        }
-		if (array_key_exists('Bcc',$headers)) {
-			$bccRecipients = $headers['Bcc'];
-			
-        }
-		
-		if (array_key_exists('Subject',$headers)) {
-			$subject = $headers['Subject'];
-			$mail_data['subject'] = $subject;
-        }
-		if($parsedBody['attachments']){
-			$mail_data['attachments'] = $parsedBody['attachments'];
-		}
-		if($parsedBody['ishtml']){
-			$mail_data['htmlbody'] = $parsedBody['html'];
-		} else {
-			$mail_data['textbody'] = $parsedBody['text'];
-		}
-		
 
-	$respObj = $zeptoAPI->sendZeptoMail($mail_data);
-	
-	if($respObj->result !== 'success'){
-		if($this->oauthconfig->getStoreConfig('zeptoFailedEmails',$this->storeId) !== null){
-			$failedEmails = json_decode($this->oauthconfig->getStoreConfig('zeptoFailedEmails',$this->storeId),true);
-		}
-		else {
-			$failedEmails = [];
-		}
-		
-		$errorCode = $respObj->details->code;
-		if($errorCode == 'SM_111' || $errorCode == 'SM_145' || $errorCode == 'SM_147') {
-			if (!in_array(strtolower($fromAddress['address']), $failedEmails)){
-				array_push($failedEmails,$fromAddress['address']);
-				$this->oauthconfig->setStoreConfig('zeptoFailedEmails',json_encode($failedEmails),$this->storeId);
-				$this->oauthconfig->flushCache();
-				$this->adminNotification = ObjectManager::getInstance()->get(AdminNotification::class);
-				$this->adminNotification->addNotice('Zoho ZeptoMail Notification',' please reconfigure zeptomail again.','',\Magento\Framework\Notification\MessageInterface::SEVERITY_NOTICE);
-			}
-		}
-		
-	}
-	
-	
-		
+
+        $message = $this->message;
+
+
+        $to = $cc = $bcc = $reply_to = array();
+        $headers = $message->getHeaders();
+
+        $fromAddress =$this:: splitNameAndAddress($headers['From']);
+
+        $mail_data = array();
+        if ($headers['From'] !== null) {
+            if (!in_array(strtolower($fromAddress['address']), $allowed_emails)){
+                $fromAddress['address'] = $this->oauthconfig->getTransmailEmailAddress(ZConstants::ident_general,$this->storeId);
+            }
+            $mail_data['from'] =  $fromAddress;
+
+        }
+
+        if ($headers['To'] !== null) {
+            $toRecipients = $headers['To'];
+            $mail_data['to'] = $this::getEmailDetails($toRecipients);
+
+        }
+        if (array_key_exists('Cc',$headers)) {
+            $ccRecipients = $headers['Cc'];
+            $mail_data['cc'] = $this::getEmailDetails($ccRecipients);
+
+        }
+        if (array_key_exists('Bcc',$headers)) {
+            $bccRecipients = $headers['Bcc'];
+
+        }
+
+        if (array_key_exists('Subject',$headers)) {
+            $subject = $headers['Subject'];
+            $mail_data['subject'] = $subject;
+        }
+        if($parsedBody['attachments']){
+            $mail_data['attachments'] = $parsedBody['attachments'];
+        }
+        if($parsedBody['ishtml']){
+            $mail_data['htmlbody'] = $parsedBody['html'];
+        } else {
+            $mail_data['textbody'] = $parsedBody['text'];
+        }
+        $respObj = $zeptoAPI->sendZeptoMail($mail_data);
+
+        if($respObj->result !== 'success'){
+            if($this->oauthconfig->getStoreConfig('zeptoFailedEmails',$this->storeId) !== null){
+                $failedEmails = json_decode($this->oauthconfig->getStoreConfig('zeptoFailedEmails',$this->storeId),true);
+            }
+            else {
+                $failedEmails = [];
+            }
+            $errorCode = $respObj->details->code;
+            if($errorCode == 'SM_111' || $errorCode == 'SM_145' || $errorCode == 'SM_147') {
+                if (!in_array(strtolower($fromAddress['address']), $failedEmails)){
+                    array_push($failedEmails,$fromAddress['address']);
+                    $this->oauthconfig->setStoreConfig('zeptoFailedEmails',json_encode($failedEmails),$this->storeId);
+                    $this->oauthconfig->flushCache();
+                    $this->adminNotification = ObjectManager::getInstance()->get(AdminNotification::class);
+                    $this->adminNotification->addNotice('Zoho ZeptoMail Notification',' please reconfigure zeptomail again.','',\Magento\Framework\Notification\MessageInterface::SEVERITY_NOTICE);
+                }
+            }
+        }
     }
-	
-	private function splitNameAndAddress($emailAddress) {
-		$result = [
-			'name' => '',
-			'address' => ''
-			];
-		$pos = strpos($emailAddress, '<');
-		if($pos !== false) {
-			$result['address'] =  substr($emailAddress, $pos+1, strlen($emailAddress)-$pos-2);
-			$result['name'] = substr($emailAddress,0,$pos-1);
-		} else {
-			$result['address'] = $emailAddress;
-		}
-		
-		return $result;
-	}
-	private function getEmailDetails($emailAddressDetails) {
-		if(!is_array($emailAddressDetails)){
-			$emailAddressDetails = explode( ',', $emailAddressDetails );
-		}
-		$result = array();
-		
-		
-		foreach($emailAddressDetails as $emailAddress){
-			$emailDetail = array();
-			$emailDetail["email_address"] = $this::splitNameAndAddress($emailAddress);
-			array_push($result,$emailDetail);
-		}
-		
-		return $result;
-	}
+
+    private function splitNameAndAddress($emailAddress) {
+        $result = [
+            'name' => '',
+            'address' => ''
+        ];
+        $pos = strpos($emailAddress, '<');
+        if($pos !== false) {
+            $result['address'] =  substr($emailAddress, $pos+1, strlen($emailAddress)-$pos-2);
+            $result['name'] = substr($emailAddress,0,$pos-1);
+        } else {
+            $result['address'] = $emailAddress;
+        }
+
+        return $result;
+    }
+    private function getEmailDetails($emailAddressDetails) {
+        if(!is_array($emailAddressDetails)){
+            $emailAddressDetails = explode( ',', $emailAddressDetails );
+        }
+        $result = array();
+
+
+        foreach($emailAddressDetails as $emailAddress){
+            $emailDetail = array();
+            $emailDetail["email_address"] = $this::splitNameAndAddress($emailAddress);
+            array_push($result,$emailDetail);
+        }
+
+        return $result;
+    }
 
 }
